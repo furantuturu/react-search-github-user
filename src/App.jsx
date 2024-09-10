22dcb62d635601320a459b988db477d157c42c47
Haemoffy <fuwansuu@gmail.com>
2024-09-10 22:51:51 +0800

    changes: - implemented useQuery for dealing with api request - seachbox and card made into component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { getGithubUser } from './api/gh-users'
+import GhUserCard from './GhUserCard'
+import SearchInputBox from './SearchInputBox'
+import GitHubIcon from './icons/github-mark-white.svg?react'
 
 export default function App() {
-  function handleSubmit(e) {
-    e.preventDefault()
-    e.stopPropagation()
-  }
+  const [searchInput, setSearchInput] = useState("")
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["githubUser", searchInput],
+    queryFn: () => getGithubUser(searchInput),
+    staleTime: 5 * 1000
+  }) 
+
+  console.log(data)
+
+  if (isPending) return <h1>Loading...</h1> 
+  if (isError) return <h1>Error Message: { error.message }</h1> 
 
   return (
     <>
       <div className="container">
-        <h1>Search a GitHub User</h1>
-        <div className="search-form">
-          <form onSubmit={ handleSubmit }>
-            <div className="searchInputBox">
-              <input type="text" required="required" />
-              <span>Enter a GitHub Username</span>
-              <button className="search-button" type="submit">
-                <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
-              </button>
-            </div>
-          </form>
+        <h1>Search a GitHub <GitHubIcon className="gh-icon" /> User</h1>
+          <SearchInputBox searchInput={ searchInput } setSearchInput={ setSearchInput } />
+        <hr />
+        <div className="gh-users-container">
+          <ul className='gh-users-list'>
+            <GhUserCard key={ data.id } userData={ data } />
+          </ul>
         </div>
       </div>
     </>
