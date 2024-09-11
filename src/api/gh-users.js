3d5a14981b619e9cbe6090a7b3ed2aa848b08f37
Haemoffy <fuwansuu@gmail.com>
2024-09-11 23:50:23 +0800

    changed from name to username

--- src/api/gh-users.js
+++ src/api/gh-users.js
@@ -4,9 +4,9 @@ const octokit = new Octokit({
     auth: import.meta.env.VITE_GITHUB_TOKEN
 })
 
-export async function getGithubUser(name) {
+export async function getGithubUser(username) {
     const response = await octokit.request('GET /users/{username}', {
-        username: name,
+        username: username,
         headers: {
             'X-Github-Api-Version': '2022-11-28'
         }
