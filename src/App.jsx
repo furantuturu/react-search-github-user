5f118f6e5d766276c57d1df3aef50de9cdad46d6
Haemoffy <fuwansuu@gmail.com>
2024-09-12 23:06:55 +0800

    going back to using useQuery ot retrieve a user

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useQueries, useQuery } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
 import { getGithubUser } from './api/gh-users'
 import GhUserCard from './GhUserCard'
 import SearchInputBox from './SearchInputBox'
@@ -9,23 +9,13 @@ import useDebounce from './customHooks/useDebounce'
 export default function App() {
   const [searchInput, setSearchInput] = useState("")
 
-  const debouncedValue = useDebounce(searchInput, 1000)
+  const debouncedInput = useDebounce(searchInput, 1000)
 
-  //* placeholder data that processes the debounced input string and return this arr
-  const names = ['f', 'fu', 'fur', 'fura', 'furan', 'furant', 'furantu', 'furantut', 'furantutu', 'furantutur', 'furantuturu']
-
-  const queries = useQueries({
-    queries: names.map(char => ({
-      queryKey: ["ghuser", char],
-      queryFn: () => getGithubUser(char),
-      retry: false,
-      staleTime: 10 * 1000
-    })),
-    combine: (results) => {
-      return {
-        data: results.map(result => result.data),
-      }
-    }
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["githubUser", debouncedInput],
+    queryFn: () => getGithubUser(debouncedInput),
+    enabled: !!debouncedInput,
+    staleTime: 5 * 1000
   })
 
   return (
@@ -36,12 +26,9 @@ export default function App() {
         <hr />
         <div className="gh-users-container">
           <ul className='gh-users-list'>
-            { ( queries.data ?? "No user/s found" )
-                                                .filter(data => data !== undefined)
-                                                .map(data => {
-                                                  return <GhUserCard key={ data.id } userData={ data } />
-            }) }
-
+          { isLoading && <h1>Loading...</h1> }
+          { isError && <h1>User Not Found</h1> }
+          { data && <GhUserCard key={ data.id } userData={ data } /> }
           </ul>
         </div>
       </div>
